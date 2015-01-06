70d45ad6646ff4ef1abcaa1f82d344c53babca9e
Krzysztof Sobczak <poczta.ks@gmail.com>
2015-01-06 16:05:21 +0100

    implementation of information gain and partial implementation of IDT

--- dmp.js
+++ dmp.js
@@ -13,8 +13,31 @@ Object.deepExtend = function(destination, source) {
     return destination;
 };
 
-function sgn(x) {
-    return x > 0 ? 1 : 0;
+function I(p, n) {
+    var result = -( (p / (p + n)) * log(2, p / (p + n)) + (n / (p + n)) * log(2, n / (p + n)) );
+    return result;
+}
+
+function IP(p_high, n_high, p_low, n_low) {
+    var p = p_high + p_low;
+    var n = n_high + n_low;
+    var result = ((p_high + n_high) / (p + n)) * I(p_high, n_high) + ((p_low + n_low) / (p + n)) * I(p_low, n_low);
+    return result;
+}
+
+function InformationGain(p_high, n_high, p_low, n_low) {
+    var p = p_high + p_low;
+    var n = n_high + n_low;
+    var result = I(p, n) - IP(p_high, n_high, p_low, n_low);
+    return result;
+}
+
+function log(base, n) {
+    if(n == 0) {
+        return 0;
+    }
+    var result = Math.log(n) / Math.log(base);
+    return result;
 }
 
 function tanh(x) {
@@ -35,6 +58,11 @@ function tanh(x) {
 function tanhDerivative(x) {
     return 1 - Math.pow(x, 2);
 }
+
+function sgn(x) {
+    return (x > 0.0) ? 1.0 : -1.0;
+}
+
 
 function sigmoid(x) {
     return 1 / (1 + Math.exp(-x));
@@ -229,15 +257,120 @@ RootNode.prototype = {
 var Network = function(size, options) {
     options = options || {};
     this.rootNode = new RootNode(size);
-    this.activationFunction = options.activationFunction || sigmoid;
-    this.activationDerivative = options.activationDerivative || sigmoidDerivative;
+    this.activationFunction = options.activationFunction || sgn;
+    this.activationDerivative = options.activationDerivative || sgn;
     this.learningRate = options.learningRate || 0.1;
     this.momentum = options.momentum || 0.5;
     this.iterations = options.iterations || 1000;
 };
 
 Network.prototype = {
-    getInformationGain: function() {
+    /**
+     * Information gain needs to know results for current data set
+     * We calculate partition of good & bad classification for first & second class
+     * Then using entropy we get current information gain
+     * @param data
+     */
+    getInformationGain: function(data) {
+        var classes = [1.0,0.0];
+        var firstClass = classes[0];
+        var secondClass = classes[1];
+        // count of network output = "first class" for positive examples
+        var PositiveExamplesHighCount = 0;
+        // count of network output = "second class" for positive examples
+        var PositiveExamplesLowCount = 0;
+        // count of network output = "first class" for negative examples
+        var NegativeExamplesHighCount = 0;
+        // count of network output = "second class" for negative examples
+        var NegativeExamplesLowCount = 0;
+        // iterate all examples
+        data.forEach(function(datum) {
+            // get target class expected by the example
+            var expectedOutput = datum.output;
+            // calculate network classification for the example input
+            // datum.network is optional property used for mocking network output
+            var networkOutput = (datum.network !== "undefined") ? datum.network : run(datum.input);
+            if(expectedOutput == firstClass) {
+                // handle positive example
+                if(networkOutput == firstClass) {
+                    // update count of high network outputs
+                    PositiveExamplesHighCount++;
+                } else if (networkOutput == secondClass) {
+                    // update count of low network outputs
+                    PositiveExamplesLowCount++;
+                } else {
+                    console.log("Expected example output not within expected classes.");
+                }
+            } else if(expectedOutput == secondClass) {
+                // handle negative example
+                if(networkOutput == firstClass) {
+                    // update count of high network outputs
+                    NegativeExamplesHighCount++;
+                } else if (networkOutput == secondClass) {
+                    // update count of low network outputs
+                    NegativeExamplesLowCount++;
+                } else {
+                    console.log("Expected example output not within expected classes.");
+                }
+            } else {
+                console.log("Expected example output not within expected classes.");
+            }
+        }.bind(this));
+        var p_high = PositiveExamplesHighCount;
+        var n_high = NegativeExamplesHighCount;
+        var p_low = PositiveExamplesLowCount;
+        var n_low = NegativeExamplesLowCount;
+        return InformationGain(p_high,n_high,p_low,n_low);
+    },
+    /**
+     * Improvement driven training aka "IDT"
+     * Is used to train new children when they are added to DMP3 network.
+     * @param data
+     */
+    improvementDrivenTraining: function(data) {
+        var iterations = 1000;
+        this.informationGainTrain(data,1000);
+        this.lazyTrain(data,10,20);
+    },
+    /**
+     *
+     * @param iterations
+     */
+    informationGainTrain: function(data,iterations) {
+        // train network for a given number of iterations
+        for(var i = 0; i < iterations; i++)
+        {
+            // get current information gain for the network
+            var currentEntropy = this.getInformationGain(data);
+            // get entropy of network with 1 less negative example error
+            var negativeEntropy; // todo: implement
+            // get entropy of network with 1 less positive example error
+            var positiveEntropy; // todo: implement
+            // get information gain for 1 less negative example error
+            var negativeInformationGain = currentEntropy - negativeEntropy;
+            // get information gain for 1 less positive example error
+            var positiveInformationGain = currentEntropy - positiveEntropy;
+            // perform normalization
+            var normalizedValue = Math.max(negativeInformationGain,positiveInformationGain);
+            var adjustedNegativeInformationGain = negativeInformationGain / normalizedValue;
+            var adjustedPositiveInformationGain = positiveInformationGain / normalizedValue;
+            // randomly permutate training instances
+            // todo: implement
+            //for each incorrectly classified example e in the training set
+            //    error = CalcError(net, e)
+            //    if e.target = high let error = error * pos_err_adjust
+            //    else error = error * neg_err_adjust
+            //    // Update the nonfrozen weights with standard back propagation
+            //    UpdateNonfrozenWeights(net,error)
+            //endfor
+        }
+    },
+    /**
+     *
+     * @param iterations
+     * @param maxtries
+     */
+    lazyTrain: function(iterations,maxtries) {
 
     },
     trainWithBackpropagation: function(data) {
@@ -254,7 +387,7 @@ Network.prototype = {
                 errorSum += this.rootNode.node.error;
             }.bind(this));
             var globalError = errorSum / data.length;
-            console.log(globalError);
+            //console.log(globalError);
         }
     },
     run: function(input) {
@@ -270,21 +403,47 @@ var DMP = function(data) {
         networkCopies.push(new Network(inputSize));
         networkCopies[i].trainWithBackpropagation(data);
     }
-    console.log();
+    //console.log();
 };
 
-var data = [];
-for(var i = -2500; i < 2500; i += 5) {
-    data.push({
-        input: [i / 2500, i / 2500],
-        output: i > 0 ? 1 : 0
-    });
-}
+//var data = [];
+//for(var i = -2500; i < 2500; i += 5) {
+//    data.push({
+//        input: [i / 2500, i / 2500],
+//        output: i > 0 ? 1 : 0
+//    });
+//}
 
-data = _.shuffle(data);
+//data = _.shuffle(data);
 
 var net = new Network(2);
-net.trainWithBackpropagation(data);
-net.run([500 / 2500, 500 / 2500]);
+//net.trainWithBackpropagation(data);
+//net.run([1.0,1.0]);
+//
+//console.log(net.rootNode.node.output);
 
-console.log(net.rootNode.node.output);
+// test information gain with example of O's and X's
+console.log("---Information Gain tests---");
+data = [];
+for(var i = 0; i < 13; i ++) {
+    data.push({
+        input: 0.0,
+        output: 1.0,
+        network: 1.0
+    });
+}
+for(var j = 0; j < 25; j ++) {
+    data.push({
+        input: 0.0,
+        output: 1.0,
+        network: 0.0
+    });
+}
+for(var k = 0; k < 7; k ++) {
+    data.push({
+        input: 0.0,
+        output: 0.0,
+        network: 0.0
+    });
+}
+console.log("DMP3 example of O's and X's: " + net.getInformationGain(data));
