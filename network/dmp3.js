c31791f190c55ffbcdcdb7e88eeaf6cfe450df80
Krzysztof Sobczak <poczta.ks@gmail.com>
2015-01-11 12:55:37 +0100

    implementation of dmp3 with todos in comment

--- network/dmp3.js
+++ network/dmp3.js
@@ -9,10 +9,129 @@ var Dmp3 = function(options) {
     this.lazyTrainMaximumTries = 20;
 };
 
+/**
+ * Learn network with DMP3 algorithm
+ * @param data
+ * @return Network
+ */
 Dmp3.prototype.learn = function(data) {
     // test calls
-    var net = new Network();
-    this.improvementDrivenTraining(net,data);
+    //var net = new Network();
+    //this.improvementDrivenTraining(net,data);
+
+    /*
+    the number of hidden nodes for new children.
+     */
+    var numberOfHiddenNodes = 0;
+
+    /*
+     start with a single node perceptron network
+      */
+    var currentNetwork = new Network();
+
+    /*
+    train 3 copies of the network
+    and choose the trained network with the best information gain
+     */
+    // get initial information gain
+    var currentInformationGain = currentNetwork.getInformationGain(data);
+    // get 3 copies of the network
+    var networks = [];
+    for(var k = 0; k < 3; k++)
+    {
+        // get copy of currentNetwork
+        networks.push(currentNetwork.clone());
+    }
+    networks.forEach(function(network){
+        // train network
+        network.train(data);
+        // get information gain
+        var networkInformationGain = network.getInformationGain(data);
+        // check if information gain is better than in current network
+        if(networkInformationGain > currentInformationGain)
+        {
+            // set current best network
+            currentNetwork = network;
+            currentInformationGain = networkInformationGain;
+        }
+    });
+    networks = null;
+
+    /*
+     freeze the weights of parent_net (currentNetwork)
+     */
+    //todo: freezing
+
+    var noImprovement = 0;
+
+    do {
+
+        var newNetwork = currentNetwork.clone();
+        /*
+         allocate a left and right child with h hidden nodes each
+         connect each of these children to the root node of new_net
+         */
+        newNetwork.rootNode.childrenLayer.addChildrenNodes(data[0].input.length);
+        //todo: implement settings number of hidden nodes
+        /*
+         set left child to root node weight = 10
+         */
+        // left child is that with positive weight so index 0 in our structure
+        newNetwork.rootNode.childrenLayer.weights[0] = 10;
+        /*
+        set right child to root node weight = -10
+         */
+        // right child is that with negative weight so index 1 in our structure
+        newNetwork.rootNode.childrenLayer.weights[1] = -10;
+
+        /*
+         train 3 copies of new_net with IDT
+         */
+        // get initial information gain
+        var newNetworkInformationGain = newNetwork.getInformationGain(data);
+        // get 3 copies of the newNetwork
+        networks = [];
+        for(k = 0; k < 3; k++)
+        {
+            // get copy of newNetwork
+            networks.push(newNetwork.clone());
+        }
+        networks.forEach(function(network){
+            // train network
+            network.improvementDrivenTraining(data);
+            // get information gain
+            var networkInformationGain = network.getInformationGain(data);
+            // check if information gain is better than in new network
+            if(networkInformationGain > currentInformationGain)
+            {
+                // set current best network as newNetwork
+                newNetwork = network;
+                newNetworkInformationGain = networkInformationGain;
+            }
+        });
+        networks = null;
+
+        // // check if new network information gain is better than in current network
+        if(newNetworkInformationGain > currentInformationGain)
+        {
+            // set newNetwork as current network
+            currentNetwork = newNetwork;
+            currentInformationGain = newNetworkInformationGain;
+            noImprovement = 0;
+            /*
+             freeze the weights of parent_net (currentNetwork)
+             */
+            //todo: freezing
+        } else {
+            noImprovement += 1;
+            numberOfHiddenNodes += 1;
+        }
+
+    } while(noImprovement < 3);
+
+    // return learned network
+    return currentNetwork;
+
 };
 
 /**
